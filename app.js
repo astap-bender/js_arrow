99d984e03adc9dcc0b19c1fe1fc3f48b853b15a1
astap-bender <150295041+astap-bender@users.noreply.github.com>
2025-08-01 22:15:07 +0300

    first commit

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-document.querySelector('.arrow').addEventListener('click', function () {
-    let rotation = this.style.transform === 'rotate(45deg) translateZ(0px)' ? 'rotate(0deg) translateZ(0px)' : 'rotate(45deg) translateZ(0px)';
-    this.style.transform = rotation;
-});
+// document.querySelector('.arrow').addEventListener('click', function () {
+//     let rotation = this.style.transform === 'rotate(45deg) translateZ(0px)' ? 'rotate(0deg) translateZ(0px)' : 'rotate(45deg) translateZ(0px)';
+//     this.style.transform = rotation;
+// });
